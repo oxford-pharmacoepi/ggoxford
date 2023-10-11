4ea3b53c934b4fd6accfaf1563dc62e40bdbd8d7
Marta Alcalde-Herraiz <marta.alcalde@estudiantat.upc.edu>
2023-10-11 16:16:49 +0100

    aaa

--- extras/extras.R
+++ extras/extras.R
@@ -1,8 +1,14 @@
 
-data <- ggoxford::mockSummarisedResult()
-tab <- data
-var <- "group_level"
-strata_name <- "group_level"
+#data <- ggoxford::mockSummarisedResult()
+library(dplyr)
 
-ggplot2::ggplot(tab, ggplot2::aes(var)) +
-  ggplot2::geom_bar(fill = "strata_name")
+tab <- data
+cohort <- c('Cohort 1')
+strata1 <- 'age_group'
+
+tab <- tab %>% filter(group_level == cohort)
+if(strata1 == 'age_group'){
+  tab <- tab %>% filter(strata_name == 'age_group')
+  ggplot2::ggplot(tab, ggplot2::aes(strata_level)) +
+    ggplot2::geom_bar()
+}
--- tests/testthat/test-getBarChart.R
+++ tests/testthat/test-getBarChart.R
@@ -1,11 +1,5 @@
 test_that("result is a ggplot object", {
-  p <- getBarChart(data.frame(a=floor(runif(10, min=0, max=5))),a)
+  p <- getBarChart(data.frame(tab=floor(runif(10, min=0, max=5))),"a")
   expect_true(ggplot2::is.ggplot(p))
-
-  p <- getBarChart(tab=data.frame(a = 1),"b")
-
-
-  getBarChart(tab = data,var = "group_level")
-
-
+  expect_error(getBarChart(tab=floor(runif(10, min=0, max=5)),1))
 })
