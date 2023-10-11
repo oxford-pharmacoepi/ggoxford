461ed8d3d800ac66e5ecf202f579fe817b54153b
Braventos <braventos@idiapjgol.info>
2023-10-11 18:43:43 +0200

    third_attempt

--- R/themeOxford.R
+++ R/themeOxford.R
@@ -24,12 +24,19 @@ themeOxford <- function(plot,
     errorMessage$push(glue::glue("- {data} is not a ggplot"))
   }
 
-  base_size_check<-any(class(baseSize) %in%
-                    c("number"))
+  base_size_check<-is.numeric(baseSize)
   checkmate::assertTRUE(base_size_check, add =errorMessage)
   if (!isTRUE(base_size_check)) {
     errorMessage$push(glue::glue("- Base size is not a number"))
   }
+
+  base_size_value<- baseSize >=1 & baseSize <=100
+  checkmate::assertTRUE(base_size_value, add =errorMessage)
+  if (!isTRUE(base_size_value)) {
+    errorMessage$push(glue::glue("- Base size is too small or too large"))
+  }
+
+  checkmate::reportAssertions(collection = errorMessage)
 
   plot <- plot+
           ggplot2::theme(
--- tests/testthat/test-theme.R
+++ tests/testthat/test-theme.R
@@ -10,3 +10,15 @@ test_that("input plot", {
   expect_true(ggplot2::is.ggplot(plot))
 })
 
+test_that("input base size value", {
+  plot <-  mockSummarisedResult()%>%
+    dplyr::filter(variable=="cohort_start_date") %>%
+    ggplot2::ggplot(ggplot2::aes(x=estimate)) +
+    ggplot2::geom_bar()
+
+  expect_error(plot %>%
+    themeOxford(baseSize=299))
+
+
+})
+
