9813e0b179e85b9be5a15cec8ce6b6331fb74066
martapineda <marta.pineda1993@gmail.com>
2023-10-11 17:20:56 +0100

    Mimi helped me to pass the tests

--- R/forest_plot.R
+++ R/forest_plot.R
@@ -12,12 +12,20 @@
 #'
 #' @examples
 
-forest_plot<-function(data, outcome_labels, estimate, lowCI, highCI){
+forestPlot<-function(data, outcomelabels, estimate, lowCi, highCi){
+  errorStorage <- checkmate::makeAssertCollection()
+  checkmate::assert_data_frame(data, add = errorStorage)
+  checkmate::assert_character(outcomelabels, add = errorStorage)
+  checkmate::assert_character(lowCi, add = errorStorage)
+  checkmate::assert_character(highCi, add = errorStorage)
+  checkmate::assert_numeric(data[[lowCi]],add = errorStorage)
+  checkmate::assert_numeric(data[[highCi]],add = errorStorage)
+  checkmate::reportAssertions(errorStorage)
 
 data %>%
-  ggplot2::ggplot(ggplot2::aes(y=.data[[outcome_labels]], x=.data[[estimate]], xmin=.data[[lowCI]], xmax=.data[[highCI]])) +
+  ggplot2::ggplot(ggplot2::aes(y=.data[[outcomelabels]], x=.data[[estimate]], xmin=.data[[lowCi]], xmax=.data[[highCi]])) +
                   ggplot2::geom_pointrange() +
-                  ggplot2::geom_errorbar(ggplot2::aes(xmin=.data[[lowCI]], xmax=.data[[highCI]]), width=0.35, cex=0.5)+ # Makes whiskers on the range (more aesthetically pleasing)
+                  ggplot2::geom_errorbar(ggplot2::aes(xmin=.data[[lowCi]], xmax=.data[[highCi]]), width=0.35, cex=0.5)+ # Makes whiskers on the range (more aesthetically pleasing)
                   ggplot2::geom_vline(xintercept=1, lty=2) +  # add a dotted line at x
                   ggplot2::ylab("Outcomes") + ggplot2::xlab("Value (95% CI)") +
                   ggplot2::theme_bw()
--- tests/testthat/test-forest_plot.R
+++ tests/testthat/test-forest_plot.R
@@ -1,22 +1,37 @@
-#Loading mock data
-df = data.frame(label= paste0("X", 1:6),
-                mean = c(1.30,0.62,2.00,1.10,0.83,1.4),
-                lower = c(0.80,0.49,1.50,1.04,0.72,1.11),
-                upper = c(2.01,0.97,3.00,1.25,1.08,1.56))
-
-
-forest_plot(df,"label","mean","lower","upper")
 
 #Test
 test_that("check forest plot", {
+  #Loading mock data
+  df = data.frame(label= paste0("X", 1:6),
+                  mean = c(1.30,0.62,2.00,1.10,0.83,1.4),
+                  lower = c(0.80,0.49,1.50,1.04,0.72,1.11),
+                  upper = c(2.01,0.97,3.00,1.25,1.08,1.56))
 
-  result <- forest_plot(df,"label","mean","lower","upper")
 
+  result <- forestPlot(df,"label","mean","lower","upper")
+
+  #has the result a "gg, ggplot" class?
   expect_true(all(class(result) == c("gg","ggplot")))
 
   expect_false(all(class(result) == c("not gg","not ggplot")))
 
   expect_false(!any(class(result) == c("gg","not ggplot")))
+
+
+  #is data a data frame?
+  expect_true(all(class(result) == c("gg","ggplot")))
+
+
+  #is outcome labels a column in data?
+
+  #is estimate a column in data?
+
+  #are lowCi and highCi values?
+
+
+
+
+
 
 
 })
