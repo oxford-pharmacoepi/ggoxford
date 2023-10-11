b84d5485b53790d65602336e047f2e4bd71d6552
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2023-10-11 17:36:21 +0100

    rm test

--- tests/testthat/test-boxplot.R
+++
@@ -1,9 +0,0 @@
-test_that("make a boxplot, expect error",{
-
-  data <- mockSummarisedResult()
-  expect_error(boxplot(data, variablesToPlot = "sex"))
-  expect_error(boxplot(data, variablesToPlot = "age_group"))
-  expect_error(boxplot(data, variablesToPlot = "blood_type"))
-  expect_error(boxplot(data, variablesToPlot = "blood_type"))
-
-})
