84d4c32341408f04659abdaed166604e3f1fb566
pablosg713 <pablo.spivakovsky@gmail.com>
2023-10-28 21:15:43 +0100

    Update mockForestPlotData.R

--- R/mockForestPlotData.R
+++ R/mockForestPlotData.R
@@ -24,11 +24,12 @@ mockForestPlotData <- function(seed = 1, numVars = 8) {
     variable_level = NA,
     variable_type = "numeric",
     estimate = round((stats::runif(n = numVars, min = 0.2, max = 0.6)),2),
-    estimate_type = "Lower 95% CI",
+    estimate_type = "Central value",
     se = round((stats::runif(n = numVars, min = 0.1, max = 0.25)),2)
   )
-  rows2 <- rows1 |> dplyr::mutate(estimate = round(exp(estimate - 1.96*se),2)) |> dplyr::mutate(estimate_type = "Central value")
+  rows2 <- rows1 |> dplyr::mutate(estimate = round(exp(estimate - 1.96*se),2)) |> dplyr::mutate(estimate_type = "Lower 95% CI")
   rows3 <- rows1 |> dplyr::mutate(estimate = round(exp(estimate + 1.96*se),2)) |> dplyr::mutate(estimate_type = "Higher 95% CI")
+  rows1 <- rows1 |> dplyr::mutate(estimate = round(0.5*(exp(estimate + 1.96*se)+exp(estimate - 1.96*se)),2))
   result <- rbind(rows1,rows2,rows3)
   result <- result |> dplyr::select(-c("se")) |> dplyr::arrange(variable)
   return(result)
