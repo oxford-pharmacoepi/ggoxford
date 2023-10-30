7cf0129584159e441fd269c740806699d77adda3
pablosg713 <pablo.spivakovsky@gmail.com>
2023-10-30 17:38:08 +0000

    Update mockForestPlotData.R

--- R/mockForestPlotData.R
+++ R/mockForestPlotData.R
@@ -23,14 +23,14 @@ mockForestPlotData <- function(seed = 1, numVars = 8) {
     variable = paste0("Variable ", 1:numVars),
     variable_level = NA,
     variable_type = "numeric",
-    estimate = round((stats::runif(n = numVars, min = 0.2, max = 0.6)),2),
+    estimate = round((stats::runif(n = numVars, min = -0.6, max = 0.6)),2),
     estimate_type = "Central value",
     se = round((stats::runif(n = numVars, min = 0.1, max = 0.25)),2)
   )
   rows2 <- rows1 |> dplyr::mutate(estimate = round(exp(estimate - 1.96*se),2)) |> dplyr::mutate(estimate_type = "Lower 95% CI")
   rows3 <- rows1 |> dplyr::mutate(estimate = round(exp(estimate + 1.96*se),2)) |> dplyr::mutate(estimate_type = "Higher 95% CI")
-  rows1 <- rows1 |> dplyr::mutate(estimate = round(0.5*(exp(estimate + 1.96*se)+exp(estimate - 1.96*se)),2))
+  rows1 <- rows1 |> dplyr::mutate(estimate = round(exp(estimate),2))
   result <- rbind(rows1,rows2,rows3)
-  result <- result |> dplyr::select(-c("se")) |> dplyr::arrange(variable)
+  result <- result |> dplyr::select(-c("se")) |> dplyr::arrange(.data$variable)
   return(result)
 }
